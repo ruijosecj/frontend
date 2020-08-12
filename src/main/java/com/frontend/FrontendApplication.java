bb5159489bdbed103a0698a7b7847171b33c90d1
ruijosecj <ruijosecj@gmail.com>
2020-08-12 18:49:38 -0300

    Cliente, TipoCliente, telefones e enderecos

--- src/main/java/com/frontend/FrontendApplication.java
+++ src/main/java/com/frontend/FrontendApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.frontend.domain.Categoria;
 import com.frontend.domain.Cidade;
+import com.frontend.domain.Cliente;
+import com.frontend.domain.Endereco;
 import com.frontend.domain.Estado;
 import com.frontend.domain.Produto;
+import com.frontend.domain.enums.TipoCliente;
 import com.frontend.repositories.CategoriaRepository;
 import com.frontend.repositories.CidadeRepository;
+import com.frontend.repositories.ClienteRepository;
+import com.frontend.repositories.EnderecoRepository;
 import com.frontend.repositories.EstadoRepository;
 import com.frontend.repositories.ProdutoRepository;
 
@@ -30,6 +35,12 @@ public class FrontendApplication implements CommandLineRunner{
 	
 	@Autowired
 	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(FrontendApplication.class, args);
@@ -66,5 +77,16 @@ public class FrontendApplication implements CommandLineRunner{
 		
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1,e2));
+
 	}
 }
