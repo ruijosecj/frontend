7838037b223016a385cb1241998e40bcd115981a
ruijosecj <ruijosecj@gmail.com>
2020-08-12 16:28:13 -0300

    Estado e Cidade

--- src/main/java/com/frontend/FrontendApplication.java
+++ src/main/java/com/frontend/FrontendApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.frontend.domain.Categoria;
+import com.frontend.domain.Cidade;
+import com.frontend.domain.Estado;
 import com.frontend.domain.Produto;
 import com.frontend.repositories.CategoriaRepository;
+import com.frontend.repositories.CidadeRepository;
+import com.frontend.repositories.EstadoRepository;
 import com.frontend.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class FrontendApplication implements CommandLineRunner{
 	
 	@Autowired
 	private ProdutoRepository prodRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(FrontendApplication.class, args);
@@ -41,10 +51,20 @@ public class FrontendApplication implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
-		
 		catRepository.saveAll(Arrays.asList(cat1,cat2));
 		prodRepository.saveAll(Arrays.asList(p1,p2,p3));
 		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 	}
-
 }
