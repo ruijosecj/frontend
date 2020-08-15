b8b9a12b5eeb2ef44589cdf370c7561bede29a7c
ruijosecj <ruijosecj@gmail.com>
2020-08-15 20:42:34 -0300

    Inserindo um novo Cliente com Post

--- src/main/java/com/frontend/resources/ClienteResource.java
+++ src/main/java/com/frontend/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.frontend.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.frontend.domain.Cliente;
 import com.frontend.dto.ClienteDTO;
+import com.frontend.dto.ClienteNewDTO;
 import com.frontend.services.ClienteService;
 
 @RestController
@@ -30,6 +33,15 @@ public class ClienteResource {
 	public ResponseEntity<?> findById(@PathVariable Integer id) {
 		Cliente obj = clienteService.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDTO) {
+		Cliente obj = clienteService.fromDTO(objDTO);
+		obj = clienteService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
--- src/main/java/com/frontend/services/ClienteService.java
+++ src/main/java/com/frontend/services/ClienteService.java
@@ -10,9 +10,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.frontend.domain.Cidade;
 import com.frontend.domain.Cliente;
+import com.frontend.domain.Endereco;
+import com.frontend.domain.enums.TipoCliente;
 import com.frontend.dto.ClienteDTO;
+import com.frontend.dto.ClienteNewDTO;
 import com.frontend.repositories.ClienteRepository;
+import com.frontend.repositories.EnderecoRepository;
 import com.frontend.services.exceptions.DataIntegrityExcepion;
 import com.frontend.services.exceptions.ObjectNotFoundException;
 
@@ -21,12 +26,21 @@ public class ClienteService {
 	
 	@Autowired
 	private ClienteRepository repo;
-	
+	@Autowired
+	private EnderecoRepository endRepo;
+		
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> 
 			new ObjectNotFoundException("Objeto não encontrado! Id: " 
 		+ id + ", Tipo: " + Cliente.class.getName())); 
+	}
+	
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		endRepo.saveAll(obj.getEnderecos());
+		return obj;
 	}
 
 	public Cliente update(Cliente obj) {
@@ -61,5 +75,19 @@ public class ClienteService {
 	public Cliente fromDTO(ClienteDTO objDTO) {
 		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
 	}
-
+	
+	public Cliente fromDTO(ClienteNewDTO objDTO) {
+		Cliente cli = new Cliente(null, objDTO.getNome(), objDTO.getEmail(), objDTO.getCpfOUcnpj(), TipoCliente.toEnum(objDTO.getTipo()));
+		Cidade cid = new Cidade(objDTO.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDTO.getLogradouro(), objDTO.getNumero(), objDTO.getComplemento(), objDTO.getBairro(), objDTO.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDTO.getTelefone1());
+		if (objDTO.getTelefone2()!=null) {
+			cli.getTelefones().add(objDTO.getTelefone2());
+		}
+		if (objDTO.getTelefone3()!=null) {
+			cli.getTelefones().add(objDTO.getTelefone3());
+		}
+		return cli;
+	}
 }
