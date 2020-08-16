a69bd3083e0d7d4f3dbbc977e362de1d4c1aef5d
ruijosecj <ruijosecj@gmail.com>
2020-08-16 11:10:32 -0300

    Validacao customizada: email nao repetido atualizacao de Cliente

--- src/main/java/com/frontend/dto/ClienteDTO.java
+++ src/main/java/com/frontend/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.frontend.domain.Cliente;
+import com.frontend.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
