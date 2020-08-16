b2daadcd23f1de789e69a0816d00eff22b2220e3
ruijosecj <ruijosecj@gmail.com>
2020-08-16 14:07:47 -0300

    SubTotal e total do pedido

--- src/main/java/com/frontend/domain/Pedido.java
+++ src/main/java/com/frontend/domain/Pedido.java
@@ -54,6 +54,14 @@ public class Pedido implements Serializable{
 		this.enderecoEntrega = enderecoEntrega;
 	}
 	
+	public double getValorTotal() {
+		double soma = 0;
+		for(ItemPedido ip : itens) {
+			soma+=ip.getSubTotal();
+		}
+		return soma;
+	}
+	
 	public Integer getId() {
 		return id;
 	}
